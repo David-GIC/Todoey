df69d8683141c8cec5d92b921ea9ac1c43b4109c
git@gitlab.com:sopheadavidsopheap/pos-project-new.git <sopheadavidsopheap@gmail.com>
2019-07-17 13:34:01 +0700

    create tableview and also include data resource and delegate

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Sopheap Sopheadavid on 7/17/19.
+//  Copyright © 2019 Sopheap Sopheadavid. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController : UITableViewController {
+    
+    let itemArray = ["item1", "item2", "item3"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    //Select each row
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
