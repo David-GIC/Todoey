a5097cd11386528b285c5d8e004557b2af6b4f52
git@gitlab.com:sopheadavidsopheap/pos-project-new.git <sopheadavidsopheap@gmail.com>
2019-07-17 12:32:21 +0700

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Sopheap Sopheadavid on 7/17/19.
+//  Copyright © 2019 Sopheap Sopheadavid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
